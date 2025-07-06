b0098afdcdc009784b4eb2ef390e0b2caf164510
jeffrey chancellor <jeffreychancellor101@gmail.com>
2025-07-06 19:40:16 -0400

    wip

--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,53 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+    {
+        img: "/images/Projectors.jpg",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "/images/speakers.jpg",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "/images/microphone.jpg",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "/images/board.png",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+
+    {
+        img: "/images/sign.jpg",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
+
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+      const {payload: index} = action;
+      if(state[index]){
+        state[index].quantity++;
+      }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const {payload: index} = action;
+      if(state[index] && state[index].quantity > 0){
+        state[index].quantity--;
+      }
     },
   },
 });
